c7ec54eed4f45320aecc861120e9026f75b0eb71
Jun87 <g.jyj87@gmail.com>
2023-10-12 16:27:46 +0900

    10/12 customScrollVIew Add

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,45 +1,56 @@
-import {
-  View,
-  Text,
-  StatusBar,
-  StyleSheet,
-  Dimensions,
-  Alert,
-} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, StatusBar, StyleSheet, Dimensions} from 'react-native';
+import React, {useRef, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation} from '@react-navigation/native';
 import {FlatList, ScrollView} from 'react-native-gesture-handler';
 import {userInfo} from '../data/tempProfileData';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Entypo from 'react-native-vector-icons/Entypo';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const testData = userInfo;
+
 const Profile = () => {
   const navigation = useNavigation();
+  // 스크롤바 x축
+  const [scrollPosition, setScrollPosition] = useState(0);
+  // 스크롤바 x축 취득
+  const handleScroll = event => {
+    const {contentOffset} = event.nativeEvent;
+    const newScrollPosition = contentOffset.x;
+    setScrollPosition(newScrollPosition);
+  };
+  // 스크롤바 넓이 취득 (화면 넓비 / 페이징수 )
+  const scrollbarWidth = Dimensions.get('window').width / 2;
+  // 스크롤바 위치 취득
+  const scrollbarPosition =
+    scrollbarWidth * (scrollPosition / Dimensions.get('window').width);
+
   const goLogin = () => {
     navigation.navigate('Login');
   };
-
-  const testData = userInfo;
-
-  const setScrollViewLocation = ({viewableItems}) => {};
 
   return (
     <SafeAreaView edges={['top']} style={{flex: 1}}>
       <StatusBar backgroundColor="white" barStyle="dark-content" />
 
       <View name="profileArea" style={styles.profileArea}>
-        <View style={styles.textBaseView}>
+        <View
+          name="userNameView"
+          style={[styles.textBaseView, {marginTop: 10}]}>
           <Text style={{fontSize: 35}}>{testData.user.userName}</Text>
         </View>
 
-        <View style={styles.textBaseView}>
+        <View name="userEmailView" style={styles.textBaseView}>
           <Text style={{fontSize: 20}}>{testData.user.userEmail}</Text>
         </View>
 
-        <View style={[styles.textBaseView, {flexDirection: 'row'}]}>
+        <View
+          name="userStatusArea"
+          style={[
+            styles.textBaseView,
+            {flexDirection: 'row', marginBottom: 20},
+          ]}>
           <View style={{alignItems: 'center'}}>
             <Text style={{fontWeight: 'bold'}}>{testData.user.postCount}</Text>
             <Text>게시물</Text>
@@ -60,12 +71,14 @@ const Profile = () => {
           </View>
         </View>
 
-        <View style={[styles.textBaseView, {flexDirection: 'row'}]}>
+        <View
+          name-="optionButtonArea"
+          style={[styles.textBaseView, {flexDirection: 'row'}]}>
           <View style={styles.baseIconView}>
             <Entypo name="shop" size={35} color="black" />
             <Text>shop</Text>
           </View>
-          <View style={[styles.baseIconView,{marginHorizontal:30}]}>
+          <View style={[styles.baseIconView, {marginHorizontal: 30}]}>
             <AntDesign name="setting" size={35} color="black" />
             <Text>설정</Text>
           </View>
@@ -80,6 +93,7 @@ const Profile = () => {
         name="userPostCommentsListArea"
         style={styles.userPostCommentsListArea}>
         <View
+          name="postAndCommentLabelView"
           style={[
             styles.textBaseView,
             {
@@ -96,50 +110,60 @@ const Profile = () => {
           </View>
         </View>
 
-        <FlatList
-          // 옵션: 가로 배치
-          horizontal={true}
-          // 옵션: 페이징 단위로 넘어감
-          pagingEnabled={true}
-          // 옵션: 스크롤바를 숨김
-          showsHorizontalScrollIndicator={false}
-          // // 옵션: 스크롤바를 상단에 위치
-          // inverted={true}
-          // index 변화가 있으면 실행됨
-          onViewableItemsChanged={({viewableItems}) =>
-            setScrollViewLocation({viewableItems})
-          }
-          data={testData.userPostAndCommentList}
-          keyExtractor={item => item.typeIndex}
-          renderItem={({item}) => {
-            // 조건문을 사용하여 type에 따라 다른 내용 렌더링
-            if (item.typeIndex === 1) {
-              return (
-                <ScrollView style={styles.postAndCommentView}>
-                  {item.postAndComment.map((i, index) => (
-                    <View key={index}>
-                      <Text>test</Text>
-                      <Text>{i.name}</Text>
-                    </View>
-                  ))}
-                </ScrollView>
-              );
-            } else if (item.typeIndex === 2) {
-              return (
-                <ScrollView style={styles.postAndCommentView}>
-                  {item.postAndComment.map((i, index) => (
-                    <View key={index}>
-                      <Text>test</Text>
-                      <Text>{i.name}</Text>
-                    </View>
-                  ))}
-                </ScrollView>
-              );
-            } else {
-              return null; // 다른 경우에는 렌더링하지 않음
-            }
-          }}
-        />
+        <View name="postAndCommentListView" style={{flex: 1}}>
+          <View
+            name="customScrollView"
+            style={[
+              styles.customScrollbar,
+              {width: scrollbarWidth, left: scrollbarPosition},
+            ]}
+          />
+
+          <FlatList
+            // 옵션: 페이징 단위로 넘어감
+            pagingEnabled={true}
+            // 옵션: 가로 배치
+            horizontal={true}
+            // 옵션: 가로 스크롤 숨김
+            showsHorizontalScrollIndicator={false}
+            // 옵션: 스크롤 이벤트 정보를 설정한 함수로 전달
+            onScroll={handleScroll}
+            // 옵션: 스크롤 감속 속도 설정
+            decelerationRate="fast"
+            // 옵션: 스크롤 이벤트 발생 빈도 설정
+            scrollEventThrottle={16}
+            data={testData.userPostAndCommentList}
+            keyExtractor={item => item.typeIndex}
+            renderItem={({item}) => {
+              // 조건문을 사용하여 type에 따라 다른 내용 렌더링
+              if (item.typeIndex === 1) {
+                return (
+                  <ScrollView style={styles.postAndCommentView}>
+                    {item.postAndComment.map((i, index) => (
+                      <View key={index}>
+                        <Text>test</Text>
+                        <Text>{i.name}</Text>
+                      </View>
+                    ))}
+                  </ScrollView>
+                );
+              } else if (item.typeIndex === 2) {
+                return (
+                  <ScrollView style={styles.postAndCommentView}>
+                    {item.postAndComment.map((i, index) => (
+                      <View key={index}>
+                        <Text>test</Text>
+                        <Text>{i.name}</Text>
+                      </View>
+                    ))}
+                  </ScrollView>
+                );
+              } else {
+                return null; // 다른 경우에는 렌더링하지 않음
+              }
+            }}
+          />
+        </View>
       </View>
 
       {/* <TouchableOpacity style={styles.roundedView} onPress={() => goLogin()}>
@@ -151,11 +175,15 @@ const Profile = () => {
 
 const screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
+  customScrollbar: {
+    height: 3,
+    backgroundColor: '#DCD8D8',
+  },
   baseIconView: {
     width: 100,
     height: 100,
     borderWidth: 2,
-    borderColor: 'black',
+    borderColor: '#DCD8D8',
     borderRadius: 15,
     padding: 10,
     alignItems: 'center',
@@ -175,11 +203,11 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
   profileArea: {
-    flex: 0.5,
+    flex: 0.37,
     alignItems: 'center',
   },
   userPostCommentsListArea: {
-    flex: 0.5,
+    flex: 0.63,
     alignItems: 'center',
     flexDirection: 'column',
   },
